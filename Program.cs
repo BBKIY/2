4f40203b7cd0aa39ce6d3a10de5b435ece620359
BBKIY <104115351+BBKIY@users.noreply.github.com>
2022-11-06 18:59:05 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace цикл_wile
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string name;
-            string zodiacSign;
-            string job;
-            int age;
-            Console.Write("Ваше имя: ");
-            name = Console.ReadLine();
-            Console.Write("Ваш возраст: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Ваш знак зодиака: ");
-            zodiacSign = Console.ReadLine();
-            Console.Write("Ваша профессия: ");
-            job = Console.ReadLine();
+            string password = "";
+            while (password == "exit")
 
-            Console.WriteLine("Вас зовут " + name + ", вам " + age + " лет. Ваш знак зодиака " + zodiacSign + ", а ваша профессия " + job);
+            {
+                Console.Write("Введите пароль: ");
+                password = Console.ReadLine();
+
+            }
         }
     }
 }
